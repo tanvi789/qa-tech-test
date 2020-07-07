81f07383e8e041b1455a4a3ce4ab3e0cb360566b
Tanvi Nanda <tanvi789@gmail.com>
2020-07-07 23:24:28 +0100

    Implemented async

--- src/test/e2e/specs/challenge_specs.js
+++ src/test/e2e/specs/challenge_specs.js
@@ -5,33 +5,31 @@ const {expect, assert} = require('chai');
 
 const getIndex = require('../test_lib/find__row_array_index');
 const Browser = require('../test_lib/base_page');
-WElements = require('../test_lib/webpage_elements');
+Navigation = require('../test_lib/webpage_actions');
 
-describe("Scroll challenge", () => {
-    it('should display submit button', () => {
-        Browser.load();
+describe("Scroll challenge", async () => {
+    it('should display submit button', async () => {
+        await Browser.load();
         /**
          * Render the Challenge
          * */
-        $("#home > div > div > button > div > div > span").click();
+        await Navigation.click_on_rendering();
         /**
          * Handle Scroll
          * */
-        const submit_button = $('#challenge > div > div > div:nth-child(8) > div > div:nth-child(7) > button > div > div > span');
-        submit_button.scrollIntoView();
-        /**
-         * Assertion to make sure submit button is displayed.
-         * */
-        assert.isTrue(submit_button.isDisplayed());
+        await Navigation.handle_scroll_bar();
     });
 });
-describe("The Challenge to find out the index", () => {
-    it('should be able submit array indexes ', () => {
-        Browser.load();
-        $("#home > div > div > button > div > div > span").click();
-        const row1_data = $('#challenge > div > div > div:nth-child(7) > div > div:nth-child(2) > table > tbody > tr:nth-child(1)').getText();
-        const row2_data = $('#challenge > div > div > div:nth-child(7) > div > div:nth-child(2) > table > tbody > tr:nth-child(2)').getText();
-        const row3_data = $('#challenge > div > div > div:nth-child(7) > div > div:nth-child(2) > table > tbody > tr:nth-child(3)').getText();
+describe("The Challenge to find out the index", async () => {
+    it('should be able submit array indexes ', async () => {
+        await Browser.load();
+        await Navigation.click_on_rendering();
+        const row1 = await $('#challenge > div > div > div:nth-child(7) > div > div:nth-child(2) > table > tbody > tr:nth-child(1)');
+        const row2 = await $('#challenge > div > div > div:nth-child(7) > div > div:nth-child(2) > table > tbody > tr:nth-child(2)');
+        const row3 = await $('#challenge > div > div > div:nth-child(7) > div > div:nth-child(2) > table > tbody > tr:nth-child(3)');
+        const row1_data = await row1.getText();
+        const row2_data = await row2.getText();
+        const row3_data = await row3.getText();
         let first_index_input = getIndex.find_index_from_which_sum_of_left_and_right_is_equal(row1_data);
         console.log("Index:", first_index_input);
         let second_index_input = getIndex.find_index_from_which_sum_of_left_and_right_is_equal(row2_data);
@@ -41,20 +39,9 @@ describe("The Challenge to find out the index", () => {
         /**
          * Submit the index values
          * */
-            // WElements.submit_first_challenge.setValue(first_index_input);
-            // WElements.submit_second_challenge.setValue(second_index_input);
-            // WElements.submit_third_challenge.setValue(third_index_input);
-            // WElements.enter_name.setValue('Tanvi Nanda');
-            // WElements.submit_button.click();
-        const submit_first_challenge = $('[data-test-id= "submit-1"]');
-        const submit_second_challenge = $('[data-test-id= "submit-2"]');
-        const submit_third_challenge = $('[data-test-id= "submit-3"]');
-        const enter_name = $('[data-test-id= "submit-4"]');
-        const submit_button = $('#challenge > div > div > div:nth-child(8) > div > div:nth-child(7) > button > div > div > span');
-        submit_first_challenge.setValue(first_index_input);
-        submit_second_challenge.setValue(second_index_input);
-        submit_third_challenge.setValue(third_index_input);
-        enter_name.setValue('Tanvi Nanda');
-        submit_button.click();
+        await Navigation.submit_results_with_name(first_index_input,
+            second_index_input,
+            third_index_input,
+            'Tanvi Nanda');
     });
 });
---
+++ src/test/e2e/test_lib/webpage_actions.js
@@ -0,0 +1,37 @@
+const {expect, assert} = require('chai');
+
+const submit_results_with_name = async (fIndex, sIndex, tIndex, name) => {
+    const submit_first_challenge = await $('[data-test-id= "submit-1"]');
+    const submit_second_challenge = await $('[data-test-id= "submit-2"]');
+    const submit_third_challenge = await $('[data-test-id= "submit-3"]');
+    const enter_name = await $('[data-test-id= "submit-4"]');
+    const submit_button = await $('#challenge > div > div > div:nth-child(8) > div > div:nth-child(7) > button > div > div > span');
+    await submit_first_challenge.setValue(fIndex);
+    await submit_second_challenge.setValue(fIndex);
+    await submit_third_challenge.setValue(tIndex);
+    await enter_name.setValue(name);
+    await submit_button.click();
+};
+
+const click_on_rendering = async () => {
+    const render_button = await $('#home > div > div > button > div > div > span');
+    await render_button.click();
+};
+
+const handle_scroll_bar = async () => {
+    const submit_button = await $('#challenge > div > div > div:nth-child(8) > div > div:nth-child(7) > button > div > div > span');
+    await submit_button.scrollIntoView();
+    /**
+     * Assertion to make sure submit button is displayed.
+     * */
+    assert.isTrue(await submit_button.isDisplayed());
+};
+
+
+
+
+module.exports = {
+    submit_results_with_name,
+    click_on_rendering,
+    handle_scroll_bar
+};
